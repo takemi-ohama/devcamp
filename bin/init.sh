4d2d825fe66ac6384fd340e85d25b0a7e60d90aa
takemi-ohama <takemi.ohama@gmail.com>
2016-01-29 18:25:14 +0000

    ignore permission

--- bin/init.sh
+++ bin/init.sh
@@ -1,5 +1,6 @@
 hostname devcamp
 git config --global credential.helper store
+git config core.filemode false
 git config --global user.email "takemi.ohama@gmail.com"
 git config --global user.name "takemi-ohama"
 
