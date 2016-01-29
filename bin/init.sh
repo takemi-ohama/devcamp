40e7c20225deba8562e66a63841cf89115c52eb8
takemi-ohama <takemi.ohama@gmail.com>
2016-01-30 03:30:12 +0900

    add init

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,8 @@
-hostname devcamp
+hostname devcampi
+export PATH="${HOST}/bin:$PATH"
 git config --global credential.helper store
 git config core.filemode false
 git config --global user.email "takemi.ohama@gmail.com"
 git config --global user.name "takemi-ohama"
 
+ln -s ~/devbase/.bash_profile
